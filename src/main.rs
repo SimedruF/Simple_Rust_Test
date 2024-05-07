9e8dccbd1b595e3a049dad7a05b926c69441834d
Simedru Florin <simedruflorin@yahoo.com>
2024-05-07 19:42:14 +0300

    If and for

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+
 fn main() {
-    println!("Hello, world!");
+    let numar = 10;
+
+    if numar > 5 {
+        println!("Numărul este mai mare decât 5.");
+    } else {
+        println!("Numărul este 5 sau mai mic.");
+    }
+
+    // Folosind `for` pentru a itera peste un interval de valori
+    for i in 1..=5 {
+        println!("{}", i);
+    }
 }
